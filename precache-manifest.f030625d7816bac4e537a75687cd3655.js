2ea22f79872d1c3faf2ac874e5f5012af08272e9
范美茹 <45160849+fanmeiru@users.noreply.github.com>
2020-10-18 11:58:22 +0800

    Add files via upload

---
+++ precache-manifest.f030625d7816bac4e537a75687cd3655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c853983c596e0d1396198cb1bf41757d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b605698d8286a093412c",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "290e36b4f9c07d30b058",
+    "url": "/static/css/main.32bd339e.chunk.css"
+  },
+  {
+    "revision": "b605698d8286a093412c",
+    "url": "/static/js/2.ad1ffdfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad1ffdfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290e36b4f9c07d30b058",
+    "url": "/static/js/main.d8fa9fec.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
